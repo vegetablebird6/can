0d186208a6b75fda9f27329db4819d16c9697e79
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-09-17 11:17:50 +0800

    Update SECU.py

--- SECU.py
+++ SECU.py
@@ -1,8 +1,8 @@
 import time
 import can
-from generateID import generateID
-from generateHC import generateHC
-from generateRS import generateRS, getbit
+from genID import generateID
+from genHC import generateHC
+from genRS import generateRS, getbit
 import encryption
 import transform
 import computerMAC
@@ -12,64 +12,67 @@ K = 18
 groupAuthKey = b'f494409468476910ce95efd1f71c8759'
 groupEnKey = b'f494409468476910ce95efd1f71c8759'
 idseed = '110111111011111'
-CTR = 0
 
 
-def simple_periodic_send(bus,ctr):
-    oseed = 22022
-    new_seed = 35617
-    new_list = generateHC(''.join([bin((new_seed)).replace('0b','')]), K)
+def simple_periodic_send(bus, ctr):
+    oldseed = 22022
+    newseed = 35617
+    newlist = generateHC(''.join([bin((newseed)).replace('0b','')]), K)
     # next chain last value
-    nextCLV = new_list.pop()
+    nextCLV = newlist.pop()
     print(nextCLV)
-    # nextCLV's MAC
-    nextCLVMAC = computerMAC.computerMAC(''.join([bin((nextCLV)).replace('0b','')]),groupAuthKey)
 
     # id 部分处理
     left_idlist = generateID(idseed, groupAuthKey)
 
-    list = generateHC(''.join([bin((oseed)).replace('0b','')]), K)
+    list = generateHC(''.join([bin((oldseed)).replace('0b','')]), K)
     print(list.pop())
     # 数据部分处理
     # 1. 得到下一个哈希种子值的最后一位
     # 2. 加密数据域
-    dataset = [[0, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1], [2, 2, 2, 2, 2, 2, 2, 2], [3, 3, 3, 3, 3, 3, 3, 3],
-               [4, 4, 4, 4, 4, 4, 4, 4], [5, 5, 5, 5, 5, 5, 5, 5], [6, 6, 6, 6, 6, 6, 6, 6], [7, 7, 7, 7, 7, 7, 7, 7],
-               [8, 8, 8, 8, 8, 8, 8, 8], [9, 9, 9, 9, 9, 9, 9, 9], [10, 10, 10, 10, 10, 10, 10, 10],
-               [11, 11, 11, 11, 11, 11, 11, 11],[12, 12, 12, 12, 12, 12, 12, 12], [13, 13, 13, 13, 13, 13, 13, 13],
-               [14, 14, 14, 14, 14, 14, 14, 14], [15, 15, 15, 15, 15, 15, 15, 15], [0, 0, 0, 0, 0, 0, 0, 0],
-               [1, 1, 1, 1, 1, 1, 1, 1]]
-
     print("Starting to send messages")
-    msg = can.Message(dlc=8, is_extended_id=True)
-    for i in range(len(dataset)):
-        left_id = left_idlist[ECU_ID]
-        data = dataset[i]
-        dlc = len(data)
-        if len(list) == 0:
-            id = (left_id << 18) + nextCLVMAC
-            oseed = new_seed
-            list = new_list
-            new_seed = generateRS(K)
-            new_list = generateHC(''.join([bin((new_seed)).replace('0b','')]), K)
-            nextCLV = new_list.pop()
-            last_bit = getbit(nextCLV, K)
-        else:
-            right_id = list.pop()
-            id = (left_id << 18) + right_id
-            last_bit = getbit(nextCLV, K - i - 1)
-        data[dlc-1] = (data[dlc-1] << 1) + last_bit
-        Enctext = encryption.aesEncrypt(groupEnKey, str(ctr))
-        data_temp = transform.datalist_to_int(data, dlc)
-        Enctext = Enctext >> ((16 - dlc) * 8)
-        Enctext = Enctext ^ data_temp
-        data = transform.int_to_datalist(Enctext, dlc)
-        msg.data = data
-        msg.arbitration_id = id
-        bus.send(msg)
-        time.sleep(1)
-        ctr += 1
-
+    with open("Dataset.txt", "r") as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            dataline = line.split()
+            data = []
+            for d in dataline:
+                data.append(int(d))
+            dlc = len(data)
+            if ctr == 256:
+                ctr = 0
+            msg = can.Message(dlc=8, is_extended_id=True)
+            left_id = left_idlist[ECU_ID]
+            if ctr % K == K - 1:
+                print("computer nextCLV's MAC")
+                # nextCLV's MAC
+                nextCLVMAC = computerMAC.computerMAC(''.join([bin((nextCLV)).replace('0b', '')]), groupAuthKey)
+                id = (left_id << 18) + nextCLVMAC
+                oldseed = newseed
+                list = newlist
+                newseed = generateRS(K)
+                newlist = generateHC(''.join([bin((newseed)).replace('0b', '')]), K)
+                nextCLV = newlist.pop()
+                lastbit = getbit(nextCLV, K)
+            else:
+                right_id = list.pop()
+                id = (left_id << 18) + right_id
+                lastbit = getbit(nextCLV, K - 1 - ctr % K)
+            # print(lastbit)
+            data[dlc-1] = (data[dlc-1] << 1) + lastbit
+            Enctext = encryption.aesEncrypt(groupEnKey, str(ctr))
+            data_temp = transform.datalist_to_int(data, dlc)
+            Enctext = Enctext >> ((16 - dlc) * 8)
+            Enctext = Enctext ^ data_temp
+            data = transform.int_to_datalist(Enctext, dlc)
+            msg.data = data
+            msg.arbitration_id = id
+            bus.send(msg)
+            ctr += 1
+            # print(ctr)
+            time.sleep(1)
 
 
 
@@ -80,7 +83,7 @@ def main():
     with can.Bus(interface="socketcan", channel='vcan0', bitrate=500000) as bus:
         bus.send(reset_msg)
 
-        simple_periodic_send(bus, CTR)
+        simple_periodic_send(bus, ctr=0)
 
         bus.send(reset_msg)
         #
