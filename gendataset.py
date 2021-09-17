7ae19d4a7b9d0a77a230a1340652dd6e2f28a7fa
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-09-17 11:19:12 +0800

    Add files via upload

---
+++ gendataset.py
@@ -0,0 +1,21 @@
+import genRS
+
+
+def generatedata():
+    list = []
+    for i in range(8):
+        list.append(genRS.generateRS(7))
+    return list
+
+
+def generatedataset():
+    for i in range(1000):
+        yield generatedata()
+
+
+if __name__ == "__main__":
+    with open("Dataset.txt", "w") as file:
+        for i in generatedataset():
+            for j in i:
+                file.write(str(j) + ' ')
+            file.write('\n')
