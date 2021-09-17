3c8fe471b1b792794873e4231e798787b9eb2532
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-09-17 11:18:27 +0800

    Update RECU.py

--- RECU.py
+++ RECU.py
@@ -1,7 +1,5 @@
 import can
-from generateID import generateID
-from generateHC import generateHC
-from generateRS import generateRS, getbit
+from genID import generateID
 import encryption
 import transform
 import computerMAC
@@ -16,21 +14,20 @@ groupEnKey = b'f494409468476910ce95efd1f71c8759'
 oseed = 22022
 idseed = '110111111011111'
 
+
 def receive(bus, ctr):
     """The loop for receiving."""
     # some known value
     RHV = 116701
     last_bit = 1
-    count = 1
     # generate the leftid list
     leftidlist = generateID(idseed, groupAuthKey)
     # next hash chain's last value
     nextCLV = last_bit
     print("Start receiving messages")
     while True:
-        # if count == K + 1:
-        #     nextCLV = 0
-        #     count = 1
+        if ctr == 256:
+            ctr = 0
         # choose the leftid
         leftid = leftidlist[RECU_ID] << 18
         # set the receiver's filter
@@ -43,11 +40,11 @@ def receive(bus, ctr):
         rightid = rx_msg.arbitration_id - leftid
         # if
         if ctr % K == K - 1:
-            print(nextCLV)
             nextCLVMAC = computerMAC.computerMAC(''.join([bin((nextCLV)).replace('0b','')]), groupAuthKey)
             if nextCLVMAC != rightid:
                 continue
             RHV = nextCLV
+            nextCLV = 0
         else:
             if (vertifyHC.vertify(rightid, RHV) == False):
                 continue
@@ -57,15 +54,15 @@ def receive(bus, ctr):
         Enctext = Enctext >> ((16 - rx_msg.dlc) * 8)
         Enctext = Enctext ^ data_temp
         last_bit = Enctext % 2
-        nextCLV = (nextCLV << 1) + last_bit
         print(last_bit)
-        count += 1
         plaintext = Enctext - last_bit
         rx_msg.data = transform.int_to_datalist(plaintext, rx_msg.dlc)
         rx_msg.data[rx_msg.dlc - 1] = (rx_msg.data[rx_msg.dlc - 1] >> 1)
         print(rx_msg.data)
         ctr += 1
+        nextCLV = (nextCLV << 1) + last_bit
     print("Stopped receiving messages")
+
 
 if __name__ == "__main__":
     with can.Bus(interface="socketcan", channel='vcan0', bitrate=500000) as bus:
