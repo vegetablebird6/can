f562519072be0ff1a40d3c1b14955b676d190ae4
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-09-30 22:05:05 +0800

    Update genHC.py

--- genHC.py
+++ genHC.py
@@ -1,16 +1,16 @@
-import struct
+from Crypto.Hash import CMAC
+from Crypto.Cipher import AES
+import datetime
 
-from Crypto.Hash import SHA384
 
 # t为轮次
-def generateHC(oseed, k):
+def generateHC(oseed, groupAuthKey, k):
     list = []
     cur = oseed.encode('utf-8')
     # print(type(cur))
     for i in range(k):
-        h = SHA384.new(cur)
-        # h = ''.join([hex(int(h.hexdigest(),16) >> (384 - 18)).replace('0x','')]).encode(encoding='utf-8')
-        h = int(h.hexdigest(), 16) >> 366
+        h = CMAC.new(groupAuthKey,cur,ciphermod=AES,mac_len=16)
+        h = int(h.hexdigest(), 16) >> 110
         # list.append(h.decode('utf-8'))
         list.append(h)
         # cur = h
@@ -18,5 +18,23 @@ def generateHC(oseed, k):
     # print(list)
     return list
 
+# from Crypto.Hash import HMAC, SHA256
+#
+# def generateHC(oseed, groupAuthKey, k):
+#     list = []
+#     cur = oseed.encode('utf-8')
+#     # print(type(cur))
+#     for i in range(k):
+#         h = HMAC.new(groupAuthKey, cur, digestmod=SHA256)
+#         h = int(h.hexdigest(), 16) >> 238
+#         # list.append(h.decode('utf-8'))
+#         list.append(h)
+#         # cur = h
+#         cur = h.to_bytes(18,byteorder='big')
+#     print(list)
+#     return list
 
-# generateHC(oseed=b'2FEF1',k = 4)
+# begin = datetime.datetime.now()
+# print(generateHC(oseed=bin(35773)[2:],groupAuthKey = b'f494409468476910ce95efd1f71c8759',k = 18))
+# end = datetime.datetime.now()
+# print((end - begin).total_seconds())
