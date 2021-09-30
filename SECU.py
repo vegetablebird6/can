2db723673ec6fdb4b981b1f2f4a888a950c4e94d
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-09-30 22:05:47 +0800

    Update SECU.py

--- SECU.py
+++ SECU.py
@@ -1,35 +1,17 @@
-import time
-import can
-from genID import generateID
-from genHC import generateHC
-from genRS import generateRS, getbit
-import encryption
-import transform
-import computerMAC
-
-SECU_ID = 0
-RECU_ID = 1
-K = 18
-groupAuthKey = b'f494409468476910ce95efd1f71c8759'
-groupEnKey = b'f494409468476910ce95efd1f71c8759'
-idseed = '110111111011111011'
-
-
-
 def simple_periodic_send(bus, ctr):
     count = 0
     oldseed = 22022
     newseed = 35617
-    newlist = generateHC(bin(newseed)[2:], K)
+    newlist = generateHC(bin(newseed)[2:],groupAuthKey, K)
     # next chain last value
     nextCLV = newlist.pop()
-    print(nextCLV)
+    # print(nextCLV)
 
     # id 部分处理
     left_idlist = generateID(idseed, 0, 25, groupAuthKey)
 
-    list = generateHC(bin(oldseed)[2:], K)
-    print(list.pop())
+    list = generateHC(bin(oldseed)[2:],groupAuthKey, K)
+    # print(list.pop())
     # 数据部分处理
     # 1. 得到下一个哈希种子值的最后一位
     # 2. 加密数据域
@@ -46,6 +28,7 @@ def simple_periodic_send(bus, ctr):
             dlc = len(data)
             msg = can.Message(dlc=8, is_extended_id=True)
             left_id = left_idlist.getitem(SECU_ID)
+            #if ctr % K == K - 1:
             if count == K - 1:
                 print("computer nextCLV's MAC")
                 # nextCLV's MAC
@@ -54,7 +37,7 @@ def simple_periodic_send(bus, ctr):
                 oldseed = newseed
                 list = newlist
                 newseed = generateRS(K)
-                newlist = generateHCbin(newseed)[2:], K)
+                newlist = generateHC(bin(newseed)[2:], groupAuthKey, K)
                 nextCLV = newlist.pop()
                 lastbit = getbit(nextCLV, K)
             else:
@@ -71,28 +54,11 @@ def simple_periodic_send(bus, ctr):
             msg.data = data
             msg.arbitration_id = id
             bus.send(msg)
+            # ctr += 1
+            # if ctr == 256:
+            #     ctr = 0
             ctr = (ctr + 1) % 256
             count = (count + 1) % K
-            # print(ctr)
+            print(ctr)
             time.sleep(0.01)
     print('over')
-
-
-def main():
-    reset_msg = can.Message(
-        arbitration_id=0x00, data=[0, 0, 0, 0, 0, 0, 0, 0], is_extended_id=False
-    )
-    with can.Bus(interface="socketcan", channel='vcan0', bitrate=500000) as bus:
-        bus.send(reset_msg)
-
-        simple_periodic_send(bus, ctr=0)
-
-        bus.send(reset_msg)
-        #
-        # limited_periodic_send(bus)
-
-
-    time.sleep(2)
-
-if __name__ == '__main__':
-    main()
