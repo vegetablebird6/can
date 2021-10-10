2700ec130bebb710876f8dc6262d3c881e0f191e
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-10-10 21:47:24 +0800

    Update vertifyHC.py

--- vertifyHC.py
+++ vertifyHC.py
@@ -1,9 +1,11 @@
-from Crypto.Hash import SHA384
-def vertify(HV, RHV):
+from Crypto.Hash import CMAC
+from Crypto.Cipher import AES
+
+def vertify(HV, groupAuthKey, RHV):
     # h = SHA384.new(HV.encode('utf-8'))
     # h = ''.join([hex(int(h.hexdigest(),16) >> (384 - 18)).replace('0x','')])
-    h = SHA384.new(HV.to_bytes(18, byteorder='big'))
-    h = int(h.hexdigest(),16) >> 366
+    h = CMAC.new(groupAuthKey, HV.to_bytes(18, byteorder='big'), ciphermod=AES, mac_len=16)
+    h = int(h.hexdigest(), 16) >> 110
     if h == RHV:
         return True
     return False
