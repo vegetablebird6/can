439a1e9a3b37da2f26d35a1311d33912bca71131
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-10-12 16:43:17 +0800

    Update genRS.py

--- genRS.py
+++ genRS.py
@@ -1,8 +1,14 @@
 import random
 
+
 def generateRS(size):
     # 随机生成size位二进制
-    return random.getrandbits(size)
+    nonce = random.getrandbits(size)
+    res = bin(nonce)[2:]
+    if len(res) < size:
+        res = res.zfill(size)
+    return res
+
 
 def getbit(byte, index):
     if byte & (1 << (index - 1)):
