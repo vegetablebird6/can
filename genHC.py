9fbe0a6ca5b465bcc02b7912bfbdedcc0d5c822a
vegetablebird6 <61724823+vegetablebird6@users.noreply.github.com>
2021-11-03 19:37:57 +0800

    Update genHC.py

--- genHC.py
+++ genHC.py
@@ -1,5 +1,4 @@
-from Crypto.Hash import CMAC
-from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
 import datetime
 
 
@@ -7,13 +6,10 @@ import datetime
 def generateHC(oseed, groupAuthKey, k):
     list = []
     cur = oseed.encode('utf-8')
-    # print(type(cur))
     for i in range(k):
-        h = CMAC.new(groupAuthKey, cur, ciphermod=AES, mac_len=16)
-        h = int(h.hexdigest(), 16) >> 102
-        # list.append(h.decode('utf-8'))
+        h = SHA256.new(cur + groupAuthKey)
+        h = int(h.hexdigest(), 16) >> 231  # 256 - 25
         list.append(h)
-        # cur = h
-        cur = h.to_bytes(26, byteorder='big')
+        cur = h.to_bytes(25, byteorder='big')
     # print(list)
     return list
